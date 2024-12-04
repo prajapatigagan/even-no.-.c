097552978e74c11ff5c8bf87a1a54e3cde8aa0e4
prajapatigagan <168338988+prajapatigagan@users.noreply.github.com>
2024-12-04 19:00:57 +0530

    Add files via upload

---
+++ even odd do while loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a=50;
+
+for(a=0; a<=50; a++)
+{
+	//printf("%d print no.\n",a);
+	if(a%2==0){
+	
+	printf("%d even\n",a);
+}
+else
+printf("%d odd\n",a);
+}
+}
